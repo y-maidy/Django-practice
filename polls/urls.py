d74b573d16258682efdbde34d490c43476965b92
y_maidy <maiyasu8281@gmail.com>
2022-01-17 08:05:02 +0900

    exec: django-tutorial3

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+import debug_toolbar
+from django.urls import include, path
+from mysite import settings
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:question_id>/results/", views.results, name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
